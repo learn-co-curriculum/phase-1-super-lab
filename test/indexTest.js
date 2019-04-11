8b1f062f244ef05b16dc01743ff4f7f53101ae24
maxwellbenton <maxwellbenton@gmail.com>
2019-04-11 12:42:32 -0400

    updates tests to focus on inheritance, not private properties

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,11 +3,13 @@ describe( "Tree", () => {
 
   it( 'initializes an instance with one parameter and assigns it to `_species`', () => {
     tree = new Tree( 'Maple' )
-    expect( tree._species ).to.eql( 'Maple' )
+    expect( tree.species )
+      .to.eql( 'Maple' )
   } )
 
   it( 'has a static method `baseDefinition` that returns a sentence on trees', () => {
-    expect( Tree.definition() ).to.eql( "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves." )
+    expect( Tree.definition() )
+      .to.eql( "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves." )
   } )
 
 } )
@@ -17,12 +19,15 @@ describe( "Deciduous", () => {
 
   it( 'initializes with two parameters, species and name, and uses `super` to set `_species`', () => {
     oak = new Deciduous( 'Oak', 'Professor' )
-    expect( oak._species ).to.eql( 'Oak' )
-    expect( oak._name ).to.eql( 'Professor' )
+    expect( oak.species )
+      .to.eql( 'Oak' )
+    expect( oak.name )
+      .to.eql( 'Professor' )
   } )
 
   it( 'has a static method `definition` that uses `super.baseDefinition` and expands on it', () => {
-    expect( Deciduous.definition() ).to.eql( "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Deciduous trees shed their leaves annually." )
+    expect( Deciduous.definition() )
+      .to.eql( "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Deciduous trees shed their leaves annually." )
   } )
 
 } )
@@ -32,12 +37,15 @@ describe( "Evergreen", () => {
 
   it( 'initializes with two parameters, species and name, and uses `super` to set `_species`', () => {
     fir = new Evergreen( 'Fir', 'Douglas' )
-    expect( fir._species ).to.eql( 'Fir' )
-    expect( fir._name ).to.eql( 'Douglas' )
+    expect( fir._species )
+      .to.eql( 'Fir' )
+    expect( fir._name )
+      .to.eql( 'Douglas' )
   } )
 
   it( 'has a static method `definition` that uses `super.baseDefinition` and expands on it', () => {
-    expect( Evergreen.definition() ).to.eql( "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Evergreens keep their leaves all year round." )
+    expect( Evergreen.definition() )
+      .to.eql( "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Evergreens keep their leaves all year round." )
   } )
 
 } )
